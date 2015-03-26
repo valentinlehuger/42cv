7e2525b4b03f9af66a1c9568aa1a7c73b0382c5d
TonyR@42 <troussel@student.42.fr>
2015-03-26 14:15:59 +0100

    adapt main

--- mockup42cv/srcs/main.cpp
+++ mockup42cv/srcs/main.cpp
@@ -6,17 +6,26 @@
 //   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/03/24 15:03:51 by troussel          #+#    #+#             //
-//   Updated: 2015/03/24 15:05:53 by troussel         ###   ########.fr       //
+//   Updated: 2015/03/26 14:13:21 by troussel         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include <mainWidget.hpp>
 #include <QApplication>
+#include <iostream>
+#include <stdlib.h>
 
 int		main(int ac, char **av)
 {
 	QApplication	app(ac, av);
-	mainWidget		box;
+	mainWidget*		box;
 
+
+	if (ac != 2) {
+		std::cout << "arg error" << std::endl;
+		return (-1);
+	}
+	box = new mainWidget(av[1]);
+	delete box;
 	return app.exec();
 }
