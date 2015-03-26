76edd99e2223602d82b24bc31bbc4e38404d738b
TonyR@42 <troussel@student.42.fr>
2015-03-26 13:48:55 +0100

    init VidFRec class

---
+++ mockup42cv/includes/VidFRec.hpp
@@ -0,0 +1,41 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   VidFRec.hpp                                        :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2015/03/26 13:39:10 by troussel          #+#    #+#             //
+//   Updated: 2015/03/26 13:48:38 by troussel         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef VIDFREC_CLASS_H
+# define VIDFREC_CLASS_H
+# include <IVidFRec.hpp>
+# include <QWidget>
+# include <string>
+# include <opencv2/core/core.hpp>
+# include <opencv2/highgui/highgui.hpp>
+# include <opencv2/imgproc/imgproc.hpp>
+
+class VidFRec : public IVidFRec
+{
+	cv::Ptr<cv::FaceRecognizer>	_model;
+	cv::CascadeClassifier		_face_cascade;
+
+	cv::VideoCapture			_cap;
+
+	VidFRec(void);
+
+public:
+	VidFRec(std::string const pathToXmlHCascade);
+	virtual ~VidFRec(void);
+
+	bool				openCamera(void);
+	bool				releaseVideo(void);
+	bool				trainModelFromCsv(std::string const pathToCsv);
+	cv::Mat*			getNextFrameRGB(void);
+};
+
+#endif /* VIDFREC_CLASS_H */
